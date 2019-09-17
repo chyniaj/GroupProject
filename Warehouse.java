100549e59d6600a9f1fee7905d6bb1c86bef1c8d
klynch3 <55258675+klynch3@users.noreply.github.com>
2019-09-17 17:35:46 -0400

    Update Warehouse.java

--- Warehouse.java
+++ Warehouse.java
@@ -7,24 +7,9 @@
  */
 public class Warehouse
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Warehouse
-     */
-    public Warehouse()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(String[] args){
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public int sampleMethod(int y)
     {
         // put your code here
