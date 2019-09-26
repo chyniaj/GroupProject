294e20290c525dba8e1f66d46feaab58376c765c
chyniaj <50002105+chyniaj@users.noreply.github.com>
2019-09-26 12:39:33 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.lang.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Updating and exiting...");
+        //update warehouse code will go here
+        System.exit(0);
     }
 }
