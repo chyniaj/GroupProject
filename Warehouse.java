cabfcea6fff11e436500e1cca84a3e3dae228b11
lgeter1 <36505966+lgeter1@users.noreply.github.com>
2019-09-29 18:04:04 -0400

    Warehouse.java
    Updated with method descriptions

--- Warehouse.java
+++ Warehouse.java
@@ -1,49 +1,97 @@
-
-package com.company;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/**
+ * Write a description of class Warehouse here.
+ *
+ * @lewis (your name)
+ * @version (a version number or a date)
+ */
+public class Warehouse
+{
+    private String partName;
+    ArrayList<BikePart> inWarehouse= new ArrayList<BikePart>();
+    ArrayList<Inventory> Deliver = new ArrayList<Inventory>();
 
-public class Warehouse {
+    /**
+     * @param parameter partName- the name of the bicycle part
+     *
+     */
+    public void displayPart(String partName)//method displays bicycle part name
+    {
+        String name = partName;
+        for(int i =0; i < inWarehouse.size(); i++){
+            if(name.equals(inWarehouse.get(i).getPartName()))
+            {
+                //inWarehouse.get(i).printInfo();
+                System.out.println(
+                    inWarehouse.get(i).getPartName()+" "+
+                    inWarehouse.get(i).getPartNum()+" "+
+                    inWarehouse.get(i).getListPrice()+" "+
+                    inWarehouse.get(i).getSalePrice()+" "+
+                    inWarehouse.get(i).getSaleStatus()+" "+
+                    inWarehouse.get(i).getQuantity()//prints out each information of the bicycle
+                );
 
-   /**
-    This method will sort the ArrayList items by number order.
-   */
-    public static void sortNumber(ArrayList<BikePart> list){
-
-        int x = list.size();
-        for (int i = 0; i < x; i++){
-            for(int j = 0; j < x; j++){
-                if (list.get(i).getPartNum() > list.get(j).getPartNum()) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
+            }
+            else{
+                System.out.println(name + " is not in file");
             }
         }
 
     }
+
     /**
-     This method will sort the ArrayList items by name order.
-    */
-    public static void sortName(ArrayList<BikePart> list) {
-        int x = list.size();
-        for (int i = 0; i < x; i++){
-            for (int j = 0; j < x; j++){
-                if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) > 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
-                else if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) == 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
-                else if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) < 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
+     * @param parameter BikePart part-  bicycle part information from the arraylist BikePart
+     */
+    public void addPart( BikePart part)
+    {
+        int localpartnum = part.getPartNum();
+        int Quantity = 0;
+
+        for ( int j =0; j < inWarehouse.size(); j++)
+        {if(localpartnum != inWarehouse.get(j).getPartNum())
+            {
+                BikePart localBikePart = new BikePart(part.getPartName(), part.getPartNum(),part.getListPrice(), 
+                        part.getSalePrice(), part.getSaleStatus(), part.getQuantity());
+
+                inWarehouse.add(localBikePart);
+
             }
+            else 
+            {
+                Quantity = inWarehouse.get(j).getQuantity() + part.getQuantity();
+                BikePart localBikePart = new BikePart(part.getPartName(), part.getPartNum(),part.getListPrice(), 
+                        part.getSalePrice(), part.getSaleStatus(), part.getQuantity());
+
+                inWarehouse.set(j, localBikePart);
+
+            }
+
+        }
+
+    }
+
+    public void printInfo()
+    {
+        try{
+            System.out.println("Enter Output File: ");
+            Scanner y = new Scanner(System.in);
+            String outputfilename = y.nextLine();
+            File outputfn = new File(outputfilename);
+
+            PrintWriter outputfile = new PrintWriter(outputfn);
+
+            outputfile.println(partName);
+
+            outputfile.close(); 
+        }
+        catch(FileNotFoundException e)
+        {
+            System.err.println("File not found.");
+
         }
     }
 }
