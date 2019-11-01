78b88af6c512db1762a8c3635ceb7c7e69b7ebfb
lgeter1 <36505966+lgeter1@users.noreply.github.com>
2019-11-01 10:40:58 -0400

    Warehouse.java
    Updated Sell and Add method.
    Good to go

--- Warehouse.java
+++ Warehouse.java
@@ -1,49 +1,212 @@
-
-package com.company;
 import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
+import java.time.Instant;
+import java.lang.*;
+/**
+ * Write a description of class Warehouse here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Warehouse
+{
+    private ArrayList<BikePart> warehouse;
+    private static int numParts;
 
-public class Warehouse {
+    /**
+     * Constructor for objects of class Warehouse
+     */
+    public Warehouse()
+    {
+        warehouse = new ArrayList<BikePart>();
+        readDatabase(warehouse);
+        this.warehouse = warehouse;
+    }
 
-   /**
-    This method will sort the ArrayList items by number order.
-   */
-    public static void sortNumber(ArrayList<BikePart> list){
+    public int getNumParts(){
+        return warehouse.size();
+    }
 
-        int x = list.size();
-        for (int i = 0; i < x; i++){
-            for(int j = 0; j < x; j++){
-                if (list.get(i).getPartNum() > list.get(j).getPartNum()) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
+    public ArrayList<BikePart> getWarehouse(){
+        return this.warehouse;
+    }
+
+    /**
+     * Reads in info from warehouseDB.txt to intialize warehouse
+     *
+     * @param  inWarehouse  ArrayList that will contain list of bike parts in the warehouse
+     * @return void   
+     */
+    public static int readDatabase(ArrayList<BikePart> warehouse) throws NumberFormatException
+    {
+        try{
+            String partName;
+            int partNum;
+            double listPrice;
+            double salePrice;
+            boolean saleStatus;
+            int quantity;
+            FileInputStream fileIn = new FileInputStream("warehouseDB.txt");
+            Scanner scanIn = new Scanner(fileIn);
+
+            while(scanIn.hasNextLine()){
+                String info = scanIn.nextLine();
+                info = info.trim();
+                String[] infoHolder = info.split(",");
+                partName = infoHolder[0];
+                partNum = Integer.parseInt(infoHolder[1]);
+                listPrice = Double.parseDouble(infoHolder[2]);
+                salePrice = Double.parseDouble(infoHolder[3]);
+                saleStatus = Boolean.parseBoolean(infoHolder[4]);
+                quantity = Integer.parseInt(infoHolder[5]);
+                BikePart part = new BikePart(partName, partNum, listPrice, salePrice, saleStatus, quantity);
+                warehouse.add(part);
+                numParts++;
             }
+            fileIn.close();
         }
+        catch (FileNotFoundException e){
+            System.out.println("File Not Found. Please restart and enter new file name.");
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            System.out.print("IOException. Please restart.");
+            e.printStackTrace();
+        }
+
+        return numParts;
 
     }
+
     /**
-     This method will sort the ArrayList items by name order.
-    */
-    public static void sortName(ArrayList<BikePart> list) {
-        int x = list.size();
-        for (int i = 0; i < x; i++){
-            for (int j = 0; j < x; j++){
-                if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) > 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
+     * Displays requested part and displays a message if part is not in warehouse
+     * 
+     * @param partName, warehouse
+     */
+    public void displayPart(String partName, Warehouse warehouse)
+    {
+        String name = partName;
+        boolean done = false;
+        if(done == false){
+            for(int i =0; i <= warehouse.getWarehouse().size()-1; i++){
+                if(name.equals(warehouse.getWarehouse().get(i).getPartName()))
+                {
+                    warehouse.getWarehouse().get(i).printInfo();
+                    done = true;
                 }
-                else if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) == 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
-                else if (list.get(i).getPartName().compareTo(list.get(j).getPartName()) < 0) {
-                    BikePart temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
+                else if(i==warehouse.getWarehouse().size()-1 && done==false){
+                    System.out.println("Part is not in file."); 
                 }
             }
         }
+    }
+
+    /**
+     * Prints warehouse ArrayList to a text document
+     * 
+     * @param warehouse
+     */
+    public static void printOutWarehouseDB(Warehouse warehouse) throws FileNotFoundException, IOException{
+        FileOutputStream fileOut = new FileOutputStream("warehouseDB.txt");
+        PrintWriter out = new PrintWriter(fileOut);
+        for(int i = 0; i < warehouse.getWarehouse().size(); i++){
+            out.println(warehouse.getWarehouse().get(i).getPartName()+","+warehouse.getWarehouse().get(i).getPartNum()+","+warehouse.getWarehouse().get(i).getListPrice()+","+warehouse.getWarehouse().get(i).getSalePrice()+","+warehouse.getWarehouse().get(i).getSaleStatus()+","
+                +warehouse.getWarehouse().get(i).getQuantity());
+        }
+        out.flush();
+        fileOut.close();
+    }
+
+    /**
+     * Adds part to warehouse ArrayList
+     * 
+     * @param warehouse, part ArrayList from Warehouse and a bikepart user wishes to add
+     * @param partName, name of part that user puts in
+     * @param part, Bikepart user wishes to add 
+     */
+    public void addPart(String partName, Warehouse warehouse, BikePart part)
+    {
+        int localpartnum;
+        int Quantity = 0;
+        boolean found = false;
+
+        for ( int i=0; i < warehouse.getWarehouse().size(); i++)
+        {
+
+            if(partName.equals(warehouse.getWarehouse().get(i).getPartName()))
+            {
+
+                Quantity = warehouse.getWarehouse().get(i).getQuantity() + part.getQuantity();
+                BikePart localBikePart2= new BikePart(part.getPartName(), part.getPartNum(),part.getListPrice(),part.getSalePrice(), part.getSaleStatus(), part.getQuantity());
+
+                warehouse.getWarehouse().set(i,localBikePart2);
+                found = true;
+                break;
+            }  
+
+        }
+        if(!found){
+
+            BikePart localBikePart = new BikePart(part.getPartName(), 
+                    part.getPartNum(),
+                    part.getListPrice(), 
+                    part.getSalePrice(), 
+                    part.getSaleStatus(), 
+                    part.getQuantity());
+
+            warehouse.getWarehouse().add(localBikePart);
+
+        }
+    }
+
+    
+    /**
+     * Sells part from Inventory and prints to Warehouse
+     * @param partNum, name of the part
+     * @param number, the number of parts the user wants to sell
+     * @param warehouse, part ArrayList from Warehouse and a bikepart user wishes to adds
+     * 
+     */
+    public void Sell(int partNum,int number, Warehouse warehouse )
+    {
+        Scanner input = new Scanner(System.in);
+        BikePart part;
+        int quant2;
+
+        Instant now=Instant.now();
+        System.out.println(now.toString());
+        for(int i =0; i < warehouse.getWarehouse().size(); i++){
+            int quant1 = warehouse.getWarehouse().get(i).getQuantity();
+            if(warehouse.getWarehouse().get(i).getPartNum()==(partNum))
+            {
+                 
+                    //System.out.println(warehouse.getWarehouse().get(i).getPartName());
+                    /*+" "+
+                    warehouse.getWarehouse().get(i).getPartNum()
+                    +" "+warehouse.getWarehouse().get(i).getQuantity());*/
+                if(quant1 > number||quant1 < number||quant1 == number) 
+                {
+                    quant2 = quant1 - number;
+                    /* System.out.println(warehouse.getWarehouse().get(i).getPartName()+" "+
+                        warehouse.getWarehouse().get(i).getPartNum()
+                        +" "+ quant2);**/
+                        part = new BikePart(warehouse.getWarehouse().get(i).getPartName(), 
+                     warehouse.getWarehouse().get(i).getPartNum(),
+                     warehouse.getWarehouse().get(i).getListPrice(), 
+                     warehouse.getWarehouse().get(i).getSalePrice(), 
+                     warehouse.getWarehouse().get(i).getSaleStatus(), 
+                    quant2);
+                    warehouse.getWarehouse().set(i, part);
+
+                }
+                else {
+                    System.out.println("Not enough quantity on hand" + quant1);
+                }
+
+            }
+
+        }
+
     }
 }
